5a14cc88ad3a5cb83a8fcd73132166b46a8a8459
Alex <alex@alexseifert.com>
2021-04-02 13:45:48 +0200

    Fix SCSS build

--- packages/scss/scripts/build.ts
+++ packages/scss/scripts/build.ts
@@ -3,8 +3,8 @@ import path from 'path';
 
 async function build(): Promise<void> {
   await compileSassAndSaveMultiple({
-    sassPath: path.join(__dirname, 'src/'),
-    cssPath: path.join(__dirname, 'dist/css/'),
+    sassPath: path.join(__dirname, '..', 'src/'),
+    cssPath: path.join(__dirname, '..', 'dist/css/'),
     files: ['index.scss'],
   });
 }
