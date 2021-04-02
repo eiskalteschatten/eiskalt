76de47315b99cb831c4f3a7861224a149c51ca48
Alex <alex@alexseifert.com>
2021-04-02 13:30:09 +0200

    Create the build script for compiling the SCSS

---
+++ packages/scss/scripts/build.ts
@@ -0,0 +1,12 @@
+import { compileSassAndSaveMultiple } from 'compile-sass';
+import path from 'path';
+
+async function build(): Promise<void> {
+  await compileSassAndSaveMultiple({
+    sassPath: path.join(__dirname, 'src/'),
+    cssPath: path.join(__dirname, 'dist/css/'),
+    files: ['index.scss']
+  });
+}
+
+build();
